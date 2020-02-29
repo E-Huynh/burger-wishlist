cec9677dab872582a8b2977021a540435058a8e5
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-28 17:31:23 -0700

    Set up methods

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+//Requiring connection.js
+var connection = require("./connection.js");
+
+//ORM
+var orm = {
+    //Methods
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
+    insertOne: function(tableInput, burger_name, cb) {
+        var queryString = "INSERT INTO " + tableInput + "( burger_name ) VALUES ( " + burger_name + ");";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+              throw err;
+            }
+            cb(result);
+          });
+    },
+    updateOne: function(tableInput, id, cb) {
+        var queryString = "UPDATE " + tableInput + " SET devour = 0 WHERE id = " + id ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+              throw err;
+            }
+            cb(result);
+          });
+    },
+}
