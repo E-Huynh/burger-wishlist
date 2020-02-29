8b0e5a2c73b372665bdd6fe26e53e3992540b5c8
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-28 18:22:16 -0700

    started on routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,6 +6,15 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
 // Export routes for server.js to use.
 module.exports = router;
